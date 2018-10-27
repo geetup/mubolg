2a2522879b86c76d424d60c1819ca20917b562ce
geetup <40681096+geetup@users.noreply.github.com>
2018-10-27 15:58:51 +0800

    Initial commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Welcome extends CI_Controller {
+    public function index()
+    {
+
+
+        $this->load->model('Blog_model');
+        $blog_list = $this->Blog_model->get_blog_list();
+        $catalog_list = $this->Blog_model->get_catalog_list();
+        $this->load->view('index',array(
+            'list'=>$blog_list,
+            'catalog_list'=>$catalog_list
+        ));
+    }
+    public function reg(){
+        $this->load->view('reg');
+    }
+    public function check_email(){
+        $email = $this->input->get('email');
+        $this->load->model('User_model');
+        $rows = $this->User_model->get_user_by_email($email);
+
+    }
+}
---
+++ application/models/Blog_model.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 18/10/20
+ * Time: 下午2:49
+ */
+class Blog_model extends CI_Model
+{
+    public function get_blog_list(){
+//        $query = $this->db->get('t_blog');
+
+//        $this->db->select('*');
+//        $this->db->from('t_blog b');
+//        $this->db->join('t_blog_catalog c','b.catalog_id = c.catalog_id');
+//        $this->db->order_by('b.post_time','desc');
+//        $query = $this->db->get();
+
+        $sql = "select *,
+    (select count(*) from t_comment tc where tc.blog_id = b.blog_id) num
+     from t_blog b,t_blog_catalog c
+    where b.catalog_id = c.catalog_id
+    order by b.post_time desc";
+
+        $query = $this->db->query($sql);
+        return $query->result();
+    }
+
+
+
+    public function get_catalog_list(){
+        $query = $this->db->get('t_blog_catalog');
+        return $query->result();
+    }
+}
